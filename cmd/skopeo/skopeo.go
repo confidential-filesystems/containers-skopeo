4575ad6c31e60a065b6ca49890e73bdba5b57e59
Hui Xie <hui.742369@gmail.com>
2024-07-31 11:01:21 +0800

    Add create root command function.

--- cmd/skopeo/skopeo.go
+++ cmd/skopeo/skopeo.go
@@ -2,11 +2,18 @@ package cli
 
 import (
 	"github.com/containers/storage/pkg/reexec"
+	"github.com/spf13/cobra"
 )
 
-func Skopeo(args []string) error{
+func Skopeo(args []string) error {
 	reexec.Init()
 	rootCmd, _ := createApp()
 	rootCmd.SetArgs(args)
-    return rootCmd.Execute()
+	return rootCmd.Execute()
+}
+
+func CreateCommand() *cobra.Command {
+	reexec.Init()
+	rootCmd, _ := createApp()
+	return rootCmd
 }
